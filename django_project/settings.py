85903981416a627ccb7ba1b1bd7ebef01fce3555
Doa <doadoadaodao5@gmail.com>
2021-10-06 18:45:22 +0200

    Procfile and settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$_lpo^@xf9ed0hl1+56_3823pdiheu5cr4v-bwwd&pqk=c)ybo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -123,7 +123,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
